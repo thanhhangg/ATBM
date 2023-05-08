fcbd1bdca14bc9820de08a5f5df5f2c3c9f2ffbd
Thanh Hằng <ntthanhhang2k2@gmail.com>
2023-05-08 18:43:39 +0700

    CS5_PHONGBAN

--- Demo/UserControlPhongBanDeAn.Designer.cs
+++ Demo/UserControlPhongBanDeAn.Designer.cs
@@ -30,39 +30,43 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.dgvListUser = new System.Windows.Forms.DataGridView();
+            this.dgvDeAn = new System.Windows.Forms.DataGridView();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
-            this.dgvListRole = new System.Windows.Forms.DataGridView();
+            this.dgvPhongBan = new System.Windows.Forms.DataGridView();
             this.BTN_NS_Update = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.BTN_NS_Create = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtNewRole = new System.Windows.Forms.TextBox();
+            this.txtNewPhongBan = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.cbAddMaTP = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txt_NS_Update_tenPB = new System.Windows.Forms.TextBox();
+            this.txtUpdatePhongBan = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.pn_UpdatePB = new System.Windows.Forms.Panel();
-            this.cbUpdateMaTP = new System.Windows.Forms.ComboBox();
+            this.txtUpdateMaPB = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.pn_AddNewPB = new System.Windows.Forms.Panel();
+            this.txtUpdateTruongPhong = new System.Windows.Forms.TextBox();
+            this.txtNewTruongPhong = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDeAn)).BeginInit();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListRole)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhongBan)).BeginInit();
             this.pn_UpdatePB.SuspendLayout();
             this.pn_AddNewPB.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.dgvListUser);
-            this.panel1.Location = new System.Drawing.Point(36, 466);
+            this.panel1.Controls.Add(this.dgvDeAn);
+            this.panel1.Location = new System.Drawing.Point(40, 414);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(373, 252);
             this.panel1.TabIndex = 7;
@@ -78,25 +82,26 @@
             this.label1.TabIndex = 72;
             this.label1.Text = "DE AN";
             // 
-            // dgvListUser
+            // dgvDeAn
             // 
-            this.dgvListUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListUser.Location = new System.Drawing.Point(7, 41);
-            this.dgvListUser.Name = "dgvListUser";
-            this.dgvListUser.ReadOnly = true;
-            this.dgvListUser.RowHeadersWidth = 51;
-            this.dgvListUser.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvListUser.Size = new System.Drawing.Size(356, 202);
-            this.dgvListUser.TabIndex = 0;
+            this.dgvDeAn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDeAn.Location = new System.Drawing.Point(7, 41);
+            this.dgvDeAn.MultiSelect = false;
+            this.dgvDeAn.Name = "dgvDeAn";
+            this.dgvDeAn.ReadOnly = true;
+            this.dgvDeAn.RowHeadersWidth = 51;
+            this.dgvDeAn.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDeAn.Size = new System.Drawing.Size(356, 202);
+            this.dgvDeAn.TabIndex = 0;
             // 
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel3.Controls.Add(this.label4);
-            this.panel3.Controls.Add(this.dgvListRole);
+            this.panel3.Controls.Add(this.dgvPhongBan);
             this.panel3.Location = new System.Drawing.Point(36, 29);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(461, 405);
+            this.panel3.Size = new System.Drawing.Size(461, 355);
             this.panel3.TabIndex = 8;
             // 
             // label4
@@ -110,21 +115,24 @@
             this.label4.TabIndex = 73;
             this.label4.Text = "PHONG BAN";
             // 
-            // dgvListRole
+            // dgvPhongBan
             // 
-            this.dgvListRole.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvListRole.Location = new System.Drawing.Point(3, 41);
-            this.dgvListRole.MultiSelect = false;
-            this.dgvListRole.Name = "dgvListRole";
-            this.dgvListRole.ReadOnly = true;
-            this.dgvListRole.RowHeadersWidth = 51;
-            this.dgvListRole.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvListRole.Size = new System.Drawing.Size(453, 359);
-            this.dgvListRole.TabIndex = 0;
+            this.dgvPhongBan.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPhongBan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPhongBan.Location = new System.Drawing.Point(3, 41);
+            this.dgvPhongBan.MultiSelect = false;
+            this.dgvPhongBan.Name = "dgvPhongBan";
+            this.dgvPhongBan.ReadOnly = true;
+            this.dgvPhongBan.RowHeadersVisible = false;
+            this.dgvPhongBan.RowHeadersWidth = 51;
+            this.dgvPhongBan.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPhongBan.Size = new System.Drawing.Size(453, 309);
+            this.dgvPhongBan.TabIndex = 0;
+            this.dgvPhongBan.SelectionChanged += new System.EventHandler(this.dgvPhongBan_SelectionChanged);
             // 
             // BTN_NS_Update
             // 
-            this.BTN_NS_Update.Location = new System.Drawing.Point(202, 166);
+            this.BTN_NS_Update.Location = new System.Drawing.Point(202, 242);
             this.BTN_NS_Update.Name = "BTN_NS_Update";
             this.BTN_NS_Update.Size = new System.Drawing.Size(117, 33);
             this.BTN_NS_Update.TabIndex = 9;
@@ -162,13 +170,13 @@
             this.label3.TabIndex = 69;
             this.label3.Text = "Add a new PHONGBAN";
             // 
-            // txtNewRole
+            // txtNewPhongBan
             // 
-            this.txtNewRole.Location = new System.Drawing.Point(26, 61);
-            this.txtNewRole.MaxLength = 20;
-            this.txtNewRole.Name = "txtNewRole";
-            this.txtNewRole.Size = new System.Drawing.Size(292, 26);
-            this.txtNewRole.TabIndex = 64;
+            this.txtNewPhongBan.Location = new System.Drawing.Point(26, 61);
+            this.txtNewPhongBan.MaxLength = 20;
+            this.txtNewPhongBan.Name = "txtNewPhongBan";
+            this.txtNewPhongBan.Size = new System.Drawing.Size(292, 26);
+            this.txtNewPhongBan.TabIndex = 64;
             // 
             // label5
             // 
@@ -179,15 +187,6 @@
             this.label5.Size = new System.Drawing.Size(104, 19);
             this.label5.TabIndex = 63;
             this.label5.Text = "Tên phòng ban:";
-            // 
-            // cbAddMaTP
-            // 
-            this.cbAddMaTP.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbAddMaTP.FormattingEnabled = true;
-            this.cbAddMaTP.Location = new System.Drawing.Point(27, 122);
-            this.cbAddMaTP.Name = "cbAddMaTP";
-            this.cbAddMaTP.Size = new System.Drawing.Size(291, 28);
-            this.cbAddMaTP.TabIndex = 71;
             // 
             // label7
             // 
@@ -210,19 +209,19 @@
             this.label2.TabIndex = 69;
             this.label2.Text = "Update PHONGBAN";
             // 
-            // txt_NS_Update_tenPB
+            // txtUpdatePhongBan
             // 
-            this.txt_NS_Update_tenPB.Location = new System.Drawing.Point(27, 61);
-            this.txt_NS_Update_tenPB.MaxLength = 20;
-            this.txt_NS_Update_tenPB.Name = "txt_NS_Update_tenPB";
-            this.txt_NS_Update_tenPB.Size = new System.Drawing.Size(292, 26);
-            this.txt_NS_Update_tenPB.TabIndex = 64;
+            this.txtUpdatePhongBan.Location = new System.Drawing.Point(27, 133);
+            this.txtUpdatePhongBan.MaxLength = 20;
+            this.txtUpdatePhongBan.Name = "txtUpdatePhongBan";
+            this.txtUpdatePhongBan.Size = new System.Drawing.Size(292, 26);
+            this.txtUpdatePhongBan.TabIndex = 64;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(23, 39);
+            this.label6.Location = new System.Drawing.Point(23, 111);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(104, 19);
             this.label6.TabIndex = 63;
@@ -231,31 +230,43 @@
             // pn_UpdatePB
             // 
             this.pn_UpdatePB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pn_UpdatePB.Controls.Add(this.cbUpdateMaTP);
+            this.pn_UpdatePB.Controls.Add(this.txtUpdateTruongPhong);
+            this.pn_UpdatePB.Controls.Add(this.txtUpdateMaPB);
+            this.pn_UpdatePB.Controls.Add(this.label9);
             this.pn_UpdatePB.Controls.Add(this.label8);
             this.pn_UpdatePB.Controls.Add(this.label2);
             this.pn_UpdatePB.Controls.Add(this.BTN_NS_Update);
-            this.pn_UpdatePB.Controls.Add(this.txt_NS_Update_tenPB);
+            this.pn_UpdatePB.Controls.Add(this.txtUpdatePhongBan);
             this.pn_UpdatePB.Controls.Add(this.label6);
             this.pn_UpdatePB.Location = new System.Drawing.Point(960, 71);
             this.pn_UpdatePB.Name = "pn_UpdatePB";
-            this.pn_UpdatePB.Size = new System.Drawing.Size(359, 213);
+            this.pn_UpdatePB.Size = new System.Drawing.Size(359, 301);
             this.pn_UpdatePB.TabIndex = 75;
             // 
-            // cbUpdateMaTP
+            // txtUpdateMaPB
             // 
-            this.cbUpdateMaTP.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbUpdateMaTP.FormattingEnabled = true;
-            this.cbUpdateMaTP.Location = new System.Drawing.Point(28, 122);
-            this.cbUpdateMaTP.Name = "cbUpdateMaTP";
-            this.cbUpdateMaTP.Size = new System.Drawing.Size(291, 28);
-            this.cbUpdateMaTP.TabIndex = 73;
+            this.txtUpdateMaPB.Location = new System.Drawing.Point(27, 70);
+            this.txtUpdateMaPB.MaxLength = 20;
+            this.txtUpdateMaPB.Name = "txtUpdateMaPB";
+            this.txtUpdateMaPB.ReadOnly = true;
+            this.txtUpdateMaPB.Size = new System.Drawing.Size(292, 26);
+            this.txtUpdateMaPB.TabIndex = 75;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(23, 48);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 19);
+            this.label9.TabIndex = 74;
+            this.label9.Text = "Mã phòng ban:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(24, 100);
+            this.label8.Location = new System.Drawing.Point(23, 173);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(99, 19);
             this.label8.TabIndex = 72;
@@ -264,21 +275,48 @@
             // pn_AddNewPB
             // 
             this.pn_AddNewPB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pn_AddNewPB.Controls.Add(this.cbAddMaTP);
+            this.pn_AddNewPB.Controls.Add(this.txtNewTruongPhong);
             this.pn_AddNewPB.Controls.Add(this.label7);
             this.pn_AddNewPB.Controls.Add(this.label3);
             this.pn_AddNewPB.Controls.Add(this.BTN_NS_Create);
-            this.pn_AddNewPB.Controls.Add(this.txtNewRole);
+            this.pn_AddNewPB.Controls.Add(this.txtNewPhongBan);
             this.pn_AddNewPB.Controls.Add(this.label5);
             this.pn_AddNewPB.Location = new System.Drawing.Point(532, 71);
             this.pn_AddNewPB.Name = "pn_AddNewPB";
             this.pn_AddNewPB.Size = new System.Drawing.Size(359, 213);
             this.pn_AddNewPB.TabIndex = 74;
             // 
+            // txtUpdateTruongPhong
+            // 
+            this.txtUpdateTruongPhong.Location = new System.Drawing.Point(27, 195);
+            this.txtUpdateTruongPhong.MaxLength = 20;
+            this.txtUpdateTruongPhong.Name = "txtUpdateTruongPhong";
+            this.txtUpdateTruongPhong.Size = new System.Drawing.Size(292, 26);
+            this.txtUpdateTruongPhong.TabIndex = 76;
+            // 
+            // txtNewTruongPhong
+            // 
+            this.txtNewTruongPhong.Location = new System.Drawing.Point(27, 122);
+            this.txtNewTruongPhong.MaxLength = 20;
+            this.txtNewTruongPhong.Name = "txtNewTruongPhong";
+            this.txtNewTruongPhong.Size = new System.Drawing.Size(292, 26);
+            this.txtNewTruongPhong.TabIndex = 77;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Demo.Properties.Resources.image_removebg_preview__7_;
+            this.pictureBox1.Location = new System.Drawing.Point(532, 370);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(359, 267);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 76;
+            this.pictureBox1.TabStop = false;
+            // 
             // UserControlPhongBanDeAn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pn_UpdatePB);
             this.Controls.Add(this.pn_AddNewPB);
             this.Controls.Add(this.button2);
@@ -292,39 +330,43 @@
             this.VisibleChanged += new System.EventHandler(this.UserControlPhongBanDeAn_VisibleChanged);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDeAn)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvListRole)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhongBan)).EndInit();
             this.pn_UpdatePB.ResumeLayout(false);
             this.pn_UpdatePB.PerformLayout();
             this.pn_AddNewPB.ResumeLayout(false);
             this.pn_AddNewPB.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dgvListUser;
+        private System.Windows.Forms.DataGridView dgvDeAn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DataGridView dgvListRole;
+        private System.Windows.Forms.DataGridView dgvPhongBan;
         private System.Windows.Forms.Button BTN_NS_Update;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button BTN_NS_Create;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtNewRole;
+        private System.Windows.Forms.TextBox txtNewPhongBan;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txt_NS_Update_tenPB;
+        private System.Windows.Forms.TextBox txtUpdatePhongBan;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox cbAddMaTP;
         private System.Windows.Forms.Panel pn_UpdatePB;
         private System.Windows.Forms.Panel pn_AddNewPB;
-        private System.Windows.Forms.ComboBox cbUpdateMaTP;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUpdateMaPB;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUpdateTruongPhong;
+        private System.Windows.Forms.TextBox txtNewTruongPhong;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Demo/UserControlPhongBanDeAn.cs
+++ Demo/UserControlPhongBanDeAn.cs
@@ -25,75 +25,10 @@ namespace Demo
         private void UserControlPhongBanDeAn_Load(object sender, EventArgs e)
         {
             var phongban = ApiRequest.GetAll<OraclePhongBan>("/users/nv/phongban");
-            dgvListUser.DataSource = phongban;
+            dgvPhongBan.DataSource = phongban;
 
             var dean = ApiRequest.GetAll<OracleDeAn>("/users/nv/dean");
-            dgvListRole.DataSource = dean;
-        }
-
-        private void btnDeleteRole_Click(object sender, EventArgs e)
-        {
-            var currentRow = dgvListRole.CurrentRow;
-
-            if (currentRow == null)
-            {
-                return;
-            }
-
-            var data = new
-            {
-                role = currentRow.Cells["GrantedRole"].Value.ToString(),
-            };
-
-            string jsonBody = JsonSerializer.Serialize(data);
-
-            var result = ApiRequest.Post<object>("/users/drop-role", jsonBody);
-
-            if (result == null || result.Status == "failed")
-            {
-                MessageBox.Show("Delete role failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            else
-            {
-                MessageBox.Show("Delete role successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                var roles = ApiRequest.GetAll<OracleRole>("/users/roles");
-                dgvListRole.DataSource = roles;
-            }
-        }
-
-        private void btnDeleteUser_Click(object sender, EventArgs e)
-        {
-            var currentRow = dgvListUser.CurrentRow;
-
-            if (currentRow == null)
-            {
-                return;
-            }
-
-            var data = new
-            {
-                userId = currentRow.Cells["Username"].Value.ToString(),
-            };
-
-            string jsonBody = JsonSerializer.Serialize(data);
-
-            var result = ApiRequest.Post<object>("/users/drop-user-db", jsonBody);
-
-            if (result == null)
-            {
-                MessageBox.Show("Delete failed. An error occured.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (result.Status == "failed")
-            {
-                MessageBox.Show(result.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else
-            {
-                MessageBox.Show("Delete successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                var users = ApiRequest.GetAll<OracleUser>("/users/user-oracle");
-                dgvListUser.DataSource = users;
-            }
+            dgvDeAn.DataSource = dean;
         }
 
         private void UserControlPhongBanDeAn_VisibleChanged(object sender, EventArgs e)
@@ -104,12 +39,103 @@ namespace Demo
 
         private void BTN_NS_Create_Click(object sender, EventArgs e)
         {
+            var tenPB = txtNewPhongBan.Text;
+            var maTruongPB = txtNewTruongPhong.Text;
 
+            if (string.IsNullOrEmpty(tenPB))
+            {
+                MessageBox.Show("Tên phòng ban không thể trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(maTruongPB))
+            {
+                MessageBox.Show("Mã trưởng phòng không thể trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var result = ApiRequest.Post<object>("/users/ns/phongban/insert", JsonSerializer.Serialize(new
+            {
+                username = Auth.GetInstance().Username,
+                TENPB = tenPB,
+                TRPHG = maTruongPB
+            }));
+
+            if (result == null || result.Status == "failed")
+            {
+                MessageBox.Show($"Tạo phòng ban thất bại. {result.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dgvPhongBan.DataSource = ApiRequest.GetAll<OraclePhongBan>("/users/nv/phongban");
+                MessageBox.Show("Tạo phòng ban thành công", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtNewPhongBan.Clear();
+                txtNewTruongPhong.Clear();
+            }
         }
 
         private void BTN_NS_Update_Click(object sender, EventArgs e)
         {
+            var maPB = txtUpdateMaPB.Text;
+            var tenPB = txtUpdatePhongBan.Text;
+            var maTruongPB = txtUpdateTruongPhong.Text;
 
+            if (string.IsNullOrEmpty(maPB))
+            {
+                MessageBox.Show("Mã phòng ban không thể trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tenPB))
+            {
+                MessageBox.Show("Tên phòng ban không thể trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(maTruongPB))
+            {
+                MessageBox.Show("Mã trưởng phòng không thể trống", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var result = ApiRequest.Post<object>("/users/ns/phongban/update", JsonSerializer.Serialize(new
+            {
+                username = Auth.GetInstance().Username,
+                MAPB = maPB,
+                TENPB = tenPB,
+                TRPHG = maTruongPB
+            }));
+
+            if (result == null || result.Status == "failed")
+            {
+                MessageBox.Show($"Cập nhật phòng ban thất bại. {result.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dgvPhongBan.DataSource = ApiRequest.GetAll<OraclePhongBan>("/users/nv/phongban");
+                MessageBox.Show("Cật nhật phòng ban thành công", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtUpdatePhongBan.Clear();
+                txtUpdateMaPB.Clear();
+                txtUpdateTruongPhong.Clear();
+            }
+        }
+
+        private void dgvPhongBan_SelectionChanged(object sender, EventArgs e)
+        {
+            var currentRow = dgvPhongBan.CurrentRow;
+
+            if (currentRow == null)
+            {
+                return;
+            }
+
+            var maPB = currentRow.Cells["MAPB"].Value.ToString();
+            var tenPB = currentRow.Cells["TENPB"].Value.ToString();
+            var maTruongPB = currentRow.Cells["TRPHG"].Value.ToString();
+
+            txtUpdateMaPB.Text = maPB;
+            txtUpdatePhongBan.Text = tenPB;
+            txtUpdateTruongPhong.Text = maTruongPB;
         }
     }
 }
